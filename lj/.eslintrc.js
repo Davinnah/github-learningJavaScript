aa0a6240f4d036c2606403aa7b40394f875e8b70
Davinna <dav1nna@me.com>
2018-07-11 14:31:56 -0500

    End of Chapter 2

---
+++ lj/.eslintrc.js
@@ -0,0 +1,34 @@
+// 0=off, 1=warning, 2=error
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules": {
+        "no-unused-vars": [
+          "error", { "vars": "all", "args": "none", "ignoreRestSiblings": true }
+        ],
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console":1
+    }
+};
