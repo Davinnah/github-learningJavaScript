eb8d782e38409a41da15a9c14ae1262686589627
Davinna <dav1nna@me.com>
2018-07-27 16:53:31 -0500

    lint changes to events.js and promises.js

--- lj/es6/events.js
+++ lj/es6/events.js
@@ -1,38 +1,38 @@
 //Learning JavaScript Chapter 2: Development Tools
 "use strict";
 
-const EventEmitter = require('events').EventEmitter;
+const EventEmitter = require("events").EventEmitter;
 
 class Countdown extends EventEmitter {
-  constructor(seconds, superstitious) {
-    super();
-    this.seconds = seconds;
-    this.superstitions = !!superstitious;
-  }
-  go() {
-    const countdown = this;
-    return new Promise(function (resolve, reject) {
-      for (let i = countdown.seconds; i >= 0; i--) {
-        setTimeout(function () {
-          if(countdown.superstitious && i===13)
-              return reject(new Error("DEFINITELY NOT COUNTING THAT"));
-          countdown.emit("tick",i);
-          if(i===0) resolve();
-        },(countdown.seconds-i)*1000);
-      }
-    });
-  }
+	constructor(seconds, superstitious) {
+		super();
+		this.seconds = seconds;
+		this.superstitions = !!superstitious;
+	}
+	go() {
+		const countdown = this;
+		return new Promise(function (resolve, reject) {
+			for (let i = countdown.seconds; i >= 0; i--) {
+				setTimeout(function () {
+					if(countdown.superstitious && i===13)
+						return reject(new Error("DEFINITELY NOT COUNTING THAT"));
+					countdown.emit("tick",i);
+					if(i===0) resolve();
+				},(countdown.seconds-i)*1000);
+			}
+		});
+	}
 }
 
 const c = new Countdown(5);
 c.on("tick", function (i) {
-    if(i>0) console.log(i + "...");
-  });
+	if(i>0) console.log(i + "...");
+});
 
 c.go()
-  .then(function () {
-    console.log("GO!");
-  })
-  .catch(function (err) {
-    console.log(err.message);
-  })
+	.then(function () {
+		console.log("GO!");
+	})
+	.catch(function (err) {
+		console.log(err.message);
+	});
