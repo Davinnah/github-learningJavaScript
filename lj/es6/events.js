9032707b63e7ea334e13a954088637e5160cb8f7
Davinna <dav1nna@me.com>
2018-07-29 13:39:49 -0500

    completed event exercises

--- lj/es6/events.js
+++ lj/es6/events.js
@@ -9,29 +9,64 @@ class Countdown extends EventEmitter {
 	}
 	go() {
 		const countdown = this;
+		const timeoutIds = [];
 		return new Promise(function(resolve, reject) {
 			for(let i = countdown.seconds; i >= 0; i--) {
-				setTimeout(function() {
-					if(countdown.superstitious && i===13)
+				timeoutIds.push(setTimeout(function(){
+					if(countdown.superstitious && i===13){
+						//clear all pending timeoutIds
+						timeoutIds.forEach(clearTimeout);
 						return reject(new Error('DEFINITELY NOT COUNTING THAT'));
-					countdown.emit('tick', i);
+					}
+					else {
+						countdown.emit('tick', i);
+					}
 					if(i===0) resolve();
-				},(countdown.seconds-i)*1000);
+				},(countdown.seconds-i)*1000));
 			}
 		});
 	}
 }
 
-const c = new Countdown(5);
+function launch(){
+	return new Promise(function(resolve, reject) {
+		if(Math.random() < 0.5) return;//rocket failure
+		console.log('Lift off!');
+		setTimeout(function () {
+			resolve('In orbit!');
+		}, 2*1000); // a very fast rocket indeed
+	});
+}
+
+function addTimeout(fn, timeout){
+	if(timeout === undefined) timeout = 1000;//default timeout
+	return function (...args) {
+		return new Promise(function(resolve, reject) {
+			const tid = setTimeout(reject, timeout, new Error('promise timed out'));
+			fn(...args)
+				.then(function (...args) {
+					clearTimeout(tid);
+					resolve(...args);
+				})
+				.catch(function (...args) {
+					clearTimeout(tid);
+					reject(...args);
+				});
+		});
+	};
+}
+
+const c = new Countdown(15);
 
 c.on('tick', function(i) {
 	if(i>0) console.log(i + '...');
 });
 
 c.go()
-	.then(function() {
-		console.log('GO!');
+	.then(addTimeout(launch, 4*1000))
+	.then(function(msg) {
+		console.log(msg);
 	})
 	.catch(function(err) {
-		console.log(err.message);
+		console.log('Houston, we have a problem....'+ err.message);
 	});
