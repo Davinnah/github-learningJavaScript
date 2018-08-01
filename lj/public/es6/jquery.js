5780fcfaeb3c3e43b09cbb20de9a7a9a4a317d4a
Davinna <dav1nna@me.com>
2018-08-01 15:11:35 -0500

    last jQuery changes.

--- lj/public/es6/jquery.js
+++ lj/public/es6/jquery.js
@@ -1,26 +1,20 @@
-//Learning JavaScript Chapter 19: jQuery
+ //Learning JavaScript Chapter 19: jQuery
 
 $(function() {//shortcut to (document).ready()
   'use strict';
   //code here is run after all HTML has been loaded and the DOM is constructed
   //jQuery object that matches all <p> tags
   const $paras = $('p');
-  $('p')
-        .append('<sup>*</sup>')
-        .after('<hr>')
-        .before('<hr>');
-  $('p:even').css('color','red');
+  
+  const para2 =$('p').get(1); //second <p>
+  console.log(para2);
+  
+  const paras = $('p').get();
+  console.log(paras,$paras);
 
 
   //////////////////////////////////////////////////////////////////////////////
   //Learning JavaScript Chapter 18: JavaScript in the Browser
-  const para1 = document.getElementsByTagName('p')[0];
-  //console.log('para1: ', para1);
-  para1.textContent;
-  para1.innerHTML;
-  para1.textContent = 'Modified HTML file';
-  para1.innerHTML = '<i>Modified</i> HTML file';
-
   const highlightActions = document.querySelectorAll('[data-action="highlight"]');
   for(let a of highlightActions) {
   	a.addEventListener('click', evt => {
