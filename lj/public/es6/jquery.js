39cb38e3a5c6c0a9806f8bbd84d722ce942bcf63
Davinna <dav1nna@me.com>
2018-08-01 10:59:19 -0500

    script working

--- lj/public/es6/jquery.js
+++ lj/public/es6/jquery.js
@@ -1,7 +1,4 @@
 //Learning JavaScript Chapter 19: jQuery
-<script src="https://code.jquery.com/jquery-3.3.1.js"
-			  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-			  crossorigin="anonymous"></script>
 
 $(function() {//shortcut to (document).ready()
   'use strict';
@@ -12,5 +9,9 @@ $(function() {//shortcut to (document).ready()
   console.log(typeof $paras);
   console.log($paras instanceof $);
   console.log($paras instanceof jQuery);
+
+  //Calling jQuery with HTML creates new DOM elements
+  const $newPara = $('<p>Newly created paragraph...</p>');
+
 
 });
