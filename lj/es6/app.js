1c9e5fe82893685fae5421a8098c6925eea82878
Davinna <dav1nna@me.com>
2018-08-02 09:05:45 -0500

    end of modules section.

---
+++ lj/es6/app.js
@@ -0,0 +1,8 @@
+//Learning JavaScript Chapter 20
+//Modules
+
+const amanda = require("./amanda.js");
+const sphereVolume = require("./tyler.js");
+console.log(amanda.geometricSum(1,2,5));
+console.log(amanda.quadraticFormula(1,2,-15));
+console.log(sphereVolume(2));
